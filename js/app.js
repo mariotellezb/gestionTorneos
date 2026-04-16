56968e310e5d91bbe6fd682904d3ccf367774571
Mario Tellez <mtellez25@alumnos.uaq.mx>
2026-04-15 20:18:23 -0600

    commit 4-15

--- js/app.js
+++ js/app.js
@@ -49,7 +49,7 @@ class App {
     logout() {
         if (confirm('¿Deseas cerrar sesión?')) {
             db.logoutUser();
-            window.location.href = 'login.html';
+            window.location.href = 'index.html';
         }
     }
 
